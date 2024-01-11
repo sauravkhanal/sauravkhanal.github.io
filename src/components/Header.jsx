d677a7d6123d3975a5e75fc2e03fe4385688b045
Saurav Khanal <hi.sauravkhanal@gmail.com>
2024-01-11 09:55:56 +0545

    change cursor on hover - hero, image nondraggable

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,24 @@
 const Header = () => {
     return (
-            <nav className="flex flex-row p-4 justify-center font-medium text-lg">
-                {[ 
-                    ['Home', '#'],
-                    ['About', '#'],
-                    ['Projects', '#projects'],
-                    // ['Contact', '/contact']
-                ].map(
-                    ([title, url], index) => {
-                        return <a key={index} href={url} className="block mx-8 hover:text-action hover:underline p-1 rounded-sm-dark" >{title}</a>
-                    }
-                )
+        <nav className="flex flex-row p-4 justify-center font-medium text-lg">
+            {[
+                ['Home', '#'],
+                ['About', '#'],
+                ['Projects', '#projects'],
+                // ['Contact', '/contact']
+            ].map(
+                ([title, url], index) => {
+                    return <a
+                        key={index}
+                        href={url}
+                        className="block mx-8 hover:text-action hover:underline p-1"
+                    >
+                        {title}
+                    </a>
                 }
-            </nav>
+            )
+            }
+        </nav>
     )
 }
 
