77042794cdea23c48ee2c771861fba80a5f72e30
sauravkhanal <sauravkhanal635@gmail.com>
2024-01-25 12:23:13 +0545

    adds new project Json Api

--- src/components/data.js
+++ src/components/data.js
@@ -7,8 +7,17 @@ export const navigationItems = [
 
 export const projects = [
     {
+        name: "Json API",
+        description: "Host JSON data for free.",
+        imageURL: "https://raw.githubusercontent.com/sauravkhanal/JSONAPI/main/examplesample.png",
+        demoURL: "https://json.gorkhacloud.com",
+        sourceURL: "https://github.com/sauravkhanal/JSONAPI",
+        tools: "React, Tailwind, .NET",
+        status: 'ongoing'
+    },
+    {
         name: "Heritage Detector",
-        description: "Detects heritage site within Kathmandu Durbar Square using YOLO V8.",
+        description: "Detect heritage sites in Kathmandu Durbar Square using YOLO V8.",
         imageURL: "https://khanalsaurav.com.np/YOLO-inference-webapp/static/media/b.b24aeaf94467f360da6b.jpg",
         demoURL: "https://khanalsaurav.com.np/YOLO-inference-webapp",
         sourceURL: "https://github.com/sauravkhanal/YOLO-inference-webapp",
@@ -17,20 +26,20 @@ export const projects = [
     },
     {
         name: "License Master",
-        description: " Helps Engineering Graduate to prepare for Engineering License Exam (login/register completed)",
+        description: "Helps Engineering Graduates prepare for License Exam.",
         imageURL: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQN85gNLk8X6uQbrw5tP5SPRreKBvHXga8-tg&usqp=CAU",
         demoURL: "https://khanalsaurav.com.np/LicenseMaster/",
         sourceURL: "https://github.com/sauravkhanal/LicenseMaster",
-        tools: 'Html, Javascript, Firebase',
+        tools: 'HTML, JavaScript, Firebase',
         status: 'ongoing',
     },
     {
         name: "Signature Verification",
-        description: "Done as minor project, compares similarity of two signatures.",
+        description: "Compares similarity of two signatures (minor project).",
         imageURL: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmtIosrgub4PJen51LNRNJLdwZtqZZAvcTsZc3iB4PLiT3JIxqbvC3X17qYOOW4bBBCg&usqp=CAU',
         // demoURL: "https://github.com/sauravkhanal/Application",
         sourceURL: "https://github.com/sauravkhanal/SignatureVerificationApp",
-        tools: 'python, tensorflow, tkinter',
+        tools: 'Python, TensorFlow, Tkinter',
         status: 'completed',
     }
-]
+];
